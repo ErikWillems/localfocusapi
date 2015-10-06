7080269e29203c8b5775638536c1b03b9fc9b61d
Erik <erik@Eriks-MacBook-Pro.local>
2015-10-06 18:00:29 +0200

    added .geo() function and example

--- localfocusapi.js
+++ localfocusapi.js
@@ -1,3 +1,7 @@
+// LocalFocus API
+// Version: 0.3
+// 6 October 2015
+
 var LocalFocusAPI = (function(){
     var oldBrowser = ((!window.SVGAngle && !document.implementation.hasFeature("http://www.w3.org/TR/SVG11/feature#Shape", "1.1")) || !document.addEventListener || !document.querySelectorAll);
 
@@ -56,33 +60,27 @@ var LocalFocusAPI = (function(){
                 listen();
                 return this;
             };
-
             this.activate = function(target){
                 send({action:'activate', target:target});
                 return this;
             };
-
             this.wait = function(){
                 send({action:'wait'});
                 return this;
             };
-
             this.resume = function(){
                 send({action:'resume'});
                 return this;
             };
-
             this.element = function(){
                 return element;
             };
-
             this.getDataStore = function(callback){
                 onFunctions['special'] = callback;
                 send({action:'getDataStore'});
                 listen();
                 return this;
             };
-
             this.setDataStore = function(dataStore){
                 send({action:'setDataStore',dataStore: dataStore});
                 return this;
@@ -94,10 +92,82 @@ var LocalFocusAPI = (function(){
             this.element = noop;
             this.getDataStore = noop;
             this.setDataStore = noop;
-        }
-
-        
+        }        
     };
+
+
+    var cachedLocation = null, geoData = [];
+
+    var requestLocation = function(settings){
+        if ("geolocation" in navigator) {
+            navigator.geolocation.getCurrentPosition(function(position) {
+                var lat = position.coords.latitude,
+                lon = position.coords.longitude,
+                minDistance = Number.MAX_VALUE;
+                hideInfoBox();
+                forEach(geoData, function(){
+                    var a = Math.abs(this[1] - lat),
+                    b = Math.abs(this[0] - lon),
+                    distance = Math.sqrt(a*a+b*b);
+                    if(distance < minDistance){
+                        minDistance = distance;
+                        cachedLocation = this[2];
+                    }
+                });
+            }, function(){
+                hideInfoBox();
+                cachedLocation = false;
+            });
+        } else {
+            hideInfoBox();
+            cachedLocation = false;
+        }
+    }; 
+
+    var geoLocate = function(settings, callback, iteration){
+        if(typeof iteration === 'undefined'){
+            iteration = 0;
+        }
+        if(cachedLocation === false){
+            return;
+        }
+        if(cachedLocation === null) {
+            cachedLocation = true;
+            requestLocation(settings);
+        }
+        if(cachedLocation === true){
+            if(iteration < 30){
+                setTimeout(function(){
+                    iteration ++;
+                    geoLocate(settings, callback, iteration);
+                },1000);
+            }
+        } else if(typeof cachedLocation === 'number') {
+            callback(cachedLocation);
+        }
+    };
+
+    var forEach = function(items, callback){
+        if(!items || !callback){
+            return;
+        }
+        for (var i = 0; i < items.length; i++) {
+            var item = items[i];
+            callback.call(item, item, i);
+        };
+    }
+
+    var hideInfoBox = function(){
+        if(infoBox){
+            if(infoBox.hasAttribute('data-slideup') && window.jQuery){
+                $(infoBox).slideUp();
+            } else {
+                infoBox.style.display = 'none';
+            }
+        }
+    };
+
+    var infoBox = null;
 
     return {
         oldBrowser: function(){
@@ -118,6 +188,49 @@ var LocalFocusAPI = (function(){
                     loop.call(new widgetObject(elems[i]));
                 };
             }
+            return null;
+        },
+        geo: function(settings){
+            if(!settings || !settings.selector){
+                return;
+            }
+
+            var widgets = document.querySelectorAll(settings.selector);
+
+            if(widgets && settings['infoBox-place'] && settings['infoBox-node']){
+                var place = document.querySelector(settings['infoBox-place']);
+                if(place){
+                    infoBox = document.createElement(settings['infoBox-node']);
+                    if(settings['infoBox-class']){
+                        infoBox.setAttribute('class', settings['infoBox-class']);
+                    }
+                    if(settings['infoBox-text']){
+                        infoBox.innerText = settings['infoBox-text'];
+                    }
+                    if(settings['infoBox-html']){
+                        infoBox.innerHTML = settings['infoBox-html'];
+                    }
+                    if(settings['infoBox-slideUp'] === true){
+                        infoBox.setAttribute('data-slideup', 'true');
+                    }
+                    place.insertBefore(infoBox, place.firstChild);
+                }
+            }
+            
+            forEach(widgets, function(){
+                var o = new widgetObject(this).on('ready', function(){
+                    var widget = this;
+                    geoLocate(settings,function(location){
+                        widget.activate(location);
+                    })
+                });
+            });
+            return null;
+        },
+        loadGeo: function(data){
+            forEach(data, function(){
+                geoData.push(this);
+            });
             return null;
         }
     }
