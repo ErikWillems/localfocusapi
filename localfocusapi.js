11481f55c536d32934c3a589f58fe165098454d6
Erik <erik@localfocus.nl>
2016-12-05 16:41:25 +0100

    added click events

--- localfocusapi.js
+++ localfocusapi.js
@@ -1,6 +1,32 @@
-// LocalFocus API
-// Version: 0.3
-// 6 October 2015
+/* LocalFocus API
+    Version: 1.0
+    2 december 2016
+    By Erik Willems (twitter: @ehwillems)
+*/
+
+/*
+The MIT License (MIT)
+
+Copyright (c) 2015 LocalFocus
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
 
 var LocalFocusAPI = (function(){
     var oldBrowser = ((!window.SVGAngle && !document.implementation.hasFeature("http://www.w3.org/TR/SVG11/feature#Shape", "1.1")) || !document.addEventListener || !document.querySelectorAll);
@@ -27,9 +53,15 @@ var LocalFocusAPI = (function(){
                     }
                     var request = message.request;
                     
+                    // Listen to loaded and ready events
                     if((request.action === 'loaded' || request.action === 'ready') && typeof onFunctions[request.action] === 'function'){
                         onFunctions[request.action].call(w);
                         delete onFunctions[request.action];
+                    }
+
+                    // Listen to click events
+                    if(request.action === 'click' && typeof onFunctions[request.action] === 'function'){
+                        onFunctions[request.action].call(w, request.item);
                     }
 
                     if(request.action === 'setDataStore' && typeof onFunctions.special === 'function'){
@@ -55,9 +87,21 @@ var LocalFocusAPI = (function(){
         }
 
         if(element){
-            this.on = function(s,f){
-                onFunctions[s] = f;
-                listen();
+            this.on = function(action, func){
+                try {
+                    // .on() supports the following actions: loaded, ready or click
+                    if(action === 'loaded' && action === 'ready' && action === 'click'){
+                        throw('Not a valid action:', action);
+                    } 
+
+                    if(func === 'function'){
+                        throw('Not a valid function:', func);
+                    }
+                    onFunctions[action] = func;
+                    listen();
+                } catch(e){
+                    window.console && window.console.error(e);
+                }
                 return this;
             };
             this.activate = function(target){
